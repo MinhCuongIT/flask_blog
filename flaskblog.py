c65c50740a5c1a50e2dc34fc3db8e2f53aca2460
TRẦN MINH CƯỜNG <41182580+MinhCuongIT@users.noreply.github.com>
2020-01-09 23:01:49 +0700

    unit 3

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9cb4001f74093f5ef54e8e8ffbec3400'
 
 posts = [
     {
@@ -39,6 +41,25 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='Về chúng tôi')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account create for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Đăng kí tài khoản', form=form)
+
+@app.route('/login',methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@gmail.com' and form.password.data == 'admin':
+            flash(f'Login successful! Hello, {form.email.data}!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Login unsuccessful! Please check user infomation.', 'danger')
+    return render_template('login.html', title='Đăng nhập', form=form)
 
 
 if __name__ == "__main__":
