52c4436f41a7a0bb7b25e76d420197966021cc98
TRẦN MINH CƯỜNG <41182580+MinhCuongIT@users.noreply.github.com>
2020-01-11 22:35:47 +0700

    unit 6

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9cb4001f74093f5ef54e8e8ffbec3400'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login' #Nói cho login_manager biết rằng route nào là login
+login_manager.login_message_category = 'info' #Bootstrap required -- Please log in to access this page.
 
 from flaskblog import routes
